9bde662c223fd9156084a24a4211ba1ff55b54f7
Shivam Chawla <Akatsuki@localhost.localdomain>
2017-12-17 02:47:00 +0530

    push

--- fees_FSM.py
+++ fees_FSM.py
@@ -1,21 +1,34 @@
 from transitions import Machine
-import random
+import re
+class fees_FSM(object):
 
-class NarcolepticSuperhero(object):
-
-    states=['inital','hostel','academics','mess','rooms','undergraduation','postgraduation']
+    states=['inital','hostel','academics','mess','rooms','undergraduation','postgraduation','final']
     def __init__(self, name):
         self.name = name
-        questions={
-            "initial":"Would You like to Know Academics Fees or Hostel?"
-            "hostel":"Would You like to Know Mess Fees or Room?"
-            "academics":"Which Degree would you like to purse? UnderGraduation or PostGraduation?"
+        self.questions={
+            "initial":"Would You like to Know Academics Fees or Hostel?: ",
+            "hostel":"Would You like to Know Mess Fees or Room?: ",
+            "academics":"Which Degree would you like to purse? UnderGraduation or PostGraduation?: "
+        }
+        self.possible_states={
+            "initial":(" hostel "," academics "),
+            "hostel":(" mess "," rooms "),
+            "academics":(" undergraduation "," postgraduation ")
         }
         self.machine = Machine(model=self, states=NarcolepticSuperhero.states, initial='initial')
         self.machine.add_transition(trigger='hostel', source='initial', dest='hostel')
         self.machine.add_transition(trigger='academics', source='initial', dest='academics')
         self.machine.add_transition(trigger='mess', source='hostel', dest='mess')
         self.machine.add_transition(trigger='rooms', source='hostel', dest='rooms')
-        self.machine.add_transition(trigger='undergraduate', source='academics', dest='undergraduaion')
-        self.machine.add_transition(trigger='postgraduate', source='academics', dest='postgraduaion')
+        self.machine.add_transition(trigger='undergraduate', source='academics', dest='undergraduation')
+        self.machine.add_transition(trigger='postgraduate', source='academics', dest='postgraduayion')
     def dialouge(self):
+        while(self.state!='final'){
+            answer=str(input(self.questions[self.state]))
+            states=self.possible_states[self.state]
+            for possible_state in states:
+                if(re.match(i,ans)):
+                    print(i)
+        }
+f=fees_FSM
+f.dialouge()
