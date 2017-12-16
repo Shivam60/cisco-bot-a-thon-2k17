5c2d32b6ad9854fd9d51b156cb30428feb4fe4ef
Vinit Bodhwani <vinit.bodhwani2015@vit.ac.in>
2017-12-17 04:38:51 +0530

    State machine working

--- fees_FSM.py
+++ fees_FSM.py
@@ -1,64 +1,69 @@
 from transitions import Machine
-import random
+import re
+class fees_FSM(object):
 
-class FeesStates(object):
-
-    states=['fees','hostel','academics','mess','rooms','undergraduation','postgraduation']
+    states=['inital','hostel','academics','mess','rooms','undergraduation','postgraduation','final']
     def __init__(self, name):
         self.name = name
-        questions={
-            "fees":"Would You like to Know Academics Fees or Hostel?"
-            "hostel_fees":"Would You like to Know Mess Fees or Room?"
-
+        self.questions={
+            "initial":"Would You like to Know Academics Fees or Hostel?: ",
+            "hostel":"Would You like to Know Mess Fees or Rooms?: ",
+            "academics":"Which Degree would you like to purse? UnderGraduation or PostGraduation?: ",
+            "mess": "Fees for mess in VIT is #to be filled",
+            "rooms" : "Here is what I found. You can compare all the rooms in this link",
+            "undergraduation" : "Which one do you like to know? Btech,BSC,BCOM,BCA?",
+            "postgraduation" : "Which one do you like to know? Mtech,MSC,MCA,MBA?",
+            "btech" : "Fees for B.Tech is #TBF",
+            "bcom" : "Fees for B.Com is #TBF",
+            "bca" : "Fees for BCAis #TBF",
+            "bsc" : "Fees for BSC is #TBF",
+            "mtech" : "Fees for M.Tech is #TBF",
+            "mcom" : "Fees for M.Com is #TBF",
+            "msc" : "Fees for MSC is #TBF",
+            "mba" : "Fees for MBA is #TBF",
 
         }
-        self.machine = Machine(model=self, states=FeesStates.states, initial='fees')
-        self.machine.add_transition(trigger='hostel', source='fees', dest='hostel',
-            before='get_hostel_fees')
-        self.machine.add_transition(trigger='mess', source='hostel', dest='mess',
-            before="get_mess_fees")
-        self.machine.add_transition(trigger='rooms', source='hostel', dest='rooms',before="get_rooms_fees")
+        self.possible_states={
+            "initial":("hostel","academics"),
+            "hostel":("mess","rooms"),
+            "academics":("undergraduation","postgraduation"),
+            "undergraduation":("btech","bsc","bcom","bca"),
+            "postgraduation" : ("mtech","mcom","mca","mba")
+            
+            
+        }
+        self.machine = Machine(model=self, states=fees_FSM.states, initial='initial')
+        self.machine.add_transition(trigger='hostel', source='initial', dest='hostel')
+        self.machine.add_transition(trigger='academics', source='initial', dest='academics')
+        self.machine.add_transition(trigger='mess', source='hostel', dest='final')
+        self.machine.add_transition(trigger='rooms', source='hostel', dest='final')
+        self.machine.add_transition(trigger='undergraduation', source='academics', dest='undergraduation')
+        self.machine.add_transition(trigger='postgraduation', source='academics', dest='postgraduation')
 
-        self.machine.add_transition(trigger='academics', source='fees', dest='academics', before="get_academics_fees")
-        self.machine.add_transition(trigger='undergraduate', source='academics', dest='undergraduaion', before="get_undergraduate_fees")
-        self.machine.add_transition(trigger='postgraduate', source='academics', dest='postgraduaion', before="get_postgraduate_fees")
-        
-        self.machine.add_transition(trigger='btech', source='undergraduaion', dest='btech', before="get_btech_fees")
-        self.machine.add_transition(trigger='bsc', source='undergraduaion', dest='bsc')
-        self.machine.add_transition(trigger='bcom', source='undergraduaion', dest='bcom')
-        self.machine.add_transition(trigger='bca', source='undergraduaion', dest='bca')
+        self.machine.add_transition(trigger='btech', source='undergraduation', dest='final')
+        self.machine.add_transition(trigger='bsc', source='undergraduation', dest='final')
+        self.machine.add_transition(trigger='bcom', source='undergraduation', dest='final')
+        self.machine.add_transition(trigger='bca', source='undergraduation', dest='final')
 
-        self.machine.add_transition(trigger='mtech', source='postgraduaion', dest='mtech',before="get_mtech_fees")
-        self.machine.add_transition(trigger='mcom', source='postgraduaion', dest='mcom')
-        self.machine.add_transition(trigger='mca', source='postgraduaion', dest='mca')
-        self.machine.add_transition(trigger='mba', source='postgraduaion', dest='mba')
-
-        def get_hostel_fees(self):
-            print "Would You like to Know Mess Fees or Room?"
-
-        def get_mess_fees(self):
-            print "Here is what I found"
-
-        def get_rooms_fees(self):
-            print "Here is what I found"
-
-        def get_academics_fees(self):
-            print "Would you like to know undergraduation fees or postgraduation fees?"
-
-        def get_undergraduate_fees(self):
-            print "Which one you like to know? Btech,BSC,BCOM,BCA?"
-
-        def get_btech_fees(self):
-            print "Here is what I found"
-
-        #Similarly for others
-
-        def get_postgraduate_fees(self):
-            print "Which one you like to know? Mtech,MSC,MCA,MBA?"
-
-        def get_mtech_fees(self):
-            print "Here is what I found"
-
-        #Similarly for others.
+        self.machine.add_transition(trigger='mtech', source='postgraduation', dest='final')
+        self.machine.add_transition(trigger='mcom', source='postgraduation', dest='final')
+        self.machine.add_transition(trigger='mca', source='postgraduation', dest='final')
+        self.machine.add_transition(trigger='mba', source='postgraduation', dest='final')
 
     def dialouge(self):
+        while(self.state!='final'):
+            answer=raw_input(self.questions[str(self.state)])
+            states=self.possible_states[self.state]
+            for possible_answer in states:
+                if possible_answer in answer:
+                    eval("self."+possible_answer+"()")
+                    if(self.state == 'final'):
+                        print self.questions[str(answer)]
+                    break
+            # if answer in states:
+            #     print "YEs ITS THERE"
+            #     eval("self."+answer+"()")
+                
+        
+f=fees_FSM("fees")
+f.dialouge()
