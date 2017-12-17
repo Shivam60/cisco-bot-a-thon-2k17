b5530fbe5a166e0dcdb40f39449c171b08bba83d
Shivam Chawla <Akatsuki@localhost.localdomain>
2017-12-17 16:03:06 +0530

    Present

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ os.chdir(os.getcwd())
 logging.basicConfig()
 talkers={}
 ls=[]
-d2={}  
+st=1
 url="ws://54.245.5.208/"
 botname="mad_bot"
 key="OGU2M2NhODQtYTY2Zi00YWE1LWE5NjAtM2RjZjJlYjg4YWVjNTRkMGQwY2QtM2Fi"
@@ -38,11 +38,12 @@ def on_message(ws,message):
         botname=""
         sender=""
     if data:
+        flag=1
         botname=data["name"]
         sender=data["data"]["personEmail"]
         msgid=data["data"]["id"]
         decoded=decodemsg(msgid,key)
-        message=str(decoded[0])
+        message=str(decoded[0]).lower()
         sender=str(decoded[1])
         roomID=str(decoded[2])
         print("Message: "+message+ "\nSender:  " +sender+"\nRoom:   " +roomID+"\n key : " +decoded[3])	
@@ -50,18 +51,25 @@ def on_message(ws,message):
             t1=fees_FSM(message,roomID)
             talkers[sender]=t1
         else:
+            
             states=talkers[sender].possible_states[talkers[sender].state]
             for possible_answer in states:
+                print(states)
                 if possible_answer in message:
+                    flag=0
+                    print("matched",possible_answer)
+                    print(talkers[sender].state)
                     talkers[sender].state=possible_answer
                     if(talkers[sender].state == 'final'):
-                        print talkers[sender].questions['academics']
-                        break
-                    else:
-                        
                         postmsg(talkers[sender].room,talkers[sender].questions[talkers[sender].state],key)
-        
-
+                    else:                        
+                        postmsg(talkers[sender].room,talkers[sender].questions[talkers[sender].state],key)
+                        break
+            if flag:
+                talkers[sender].state="initial"
+                print("state changed to fallback")
+                print(talkers[sender])
+                postmsg(talkers[sender].room,talkers[sender].questions[talkers[sender].state],key)
 
 def on_error(ws, error):
     print error
@@ -149,72 +157,59 @@ var1=apiextraction("dataset.csv")
 #var1.save()
 
 print("Algorithm Trained.") 
-'''
-class FSM(object):
-    states=['fallback','final','hostel','academics','placements','fees','start','exam']
-    def __init__(self,room,roomID):
-         self.room=roomID
-         self.machine = Machine(model=self, states=FSM.states, initial='start')
-         self.ans=""
-         self.questions={
-             'start':'Let me get That for you',
-             'help':"Sorry, I can't seem to understandyou. Would you like to know the categories I deal with?",
-             'academics':"Here is what I found, academics fees is 1.5k",
-             "placements" : "Here is what I found for VIT placements. Please refer to this link : https://www.quora.com/How-is-the-placement-in-VIT-Vellore-What-is-the-average-package",
-             "exams":"VITEEE 2018 - Vellore Institute of Technology has released VITEEE 2018 application form in online and offlne mode. The last date to submit both online and offline application form of VITEEE is February 28, 2018. VIT will conduct VITEEE 2018 in online mode from April 4 to April 15, 2018. Please refer to this link for more details : https://engineering.careers360.com/articles/viteee"
-         }
-         self.possible_states={
-             'start':('academics','placements','fees','exam','hostel','help'),
-             'help':('academics','placements','fees','exam','hostel')
-         }
-         self.machine.add_transition(trigger='academics', source='start', dest='final')
-         self.machine.add_transition(trigger='placements', source='start', dest='final')
-         self.machine.add_transition(trigger='fees', source='start', dest='fees')
-         self.machine.add_transition(trigger='exam', source='start', dest='final')
-         self.machine.add_transition(trigger='hostel', source='start', dest='final')
-         self.machine.add_transition(trigger='help', source='start', dest='academics')
-         self.dialouge()
-    def dialouge(self):
-        if self.state=='start':
-            postmsg(self.room,self.questions[self.state],key)
-            postmsg(self.room,'fees?',key) 
-''' 
 class fees_FSM(object):
 
-    states=['inital','fees','hostel','academics','mess','rooms','undergraduation','postgraduation','final']
+    states=['initial','placement','fees','hostel','academics','mess','rooms','undergraduation','postgraduation','final','help']
     def __init__(self, message,room):
         self.message= message
         self.room=room
+        self.st=1
+        a="Here is a popular social media linke for your answer https://www.quora.com/What-is-the-total-4-year-fees-approx-of-VIT-for-a-student-of-rank-under-5k-10k-and-15k"
        # self.val=self.value
         self.machine = Machine(model=self, states=fees_FSM.states, initial='initial')
         self.questions={
-            "initial":"Let me get that for you. ",
-            "fees":"Which fees would you like to know? hostel,academics ",
+            "initial1":"",
+            "initial":"Hii, Let me get that for you. ",
+            "fees":"Which fees would you like to know? Hostel(including the mess) or Academics ",
             "hostel":"Would You like to Know Mess Fees or Rooms?: ",
             "academics":"Which Degree would you like to purse? UnderGraduation or PostGraduation?: ",
-            "mess": "Fees for mess in VIT is #to be filled",
-            "rooms" : "Here is what I found. You can compare all the rooms in this link",
-            "undergraduation" : "Which one do you like to know? Btech,BSC,BCOM,BCA?",
-            "postgraduation" : "Which one do you like to know? Mtech,MSC,MCA,MBA?",
-            "btech" : "Fees for B.Tech is #TBF",
-            "bcom" : "Fees for B.Com is #TBF",
-            "bca" : "Fees for BCAis #TBF",
-            "bsc" : "Fees for BSC is #TBF",
-            "mtech" : "Fees for M.Tech is #TBF",
-            "mcom" : "Fees for M.Com is #TBF",
-            "msc" : "Fees for MSC is #TBF",
-            "mba" : "Fees for MBA is #TBF",
-
+            "mess": "Fees for mess in VIT is #to be filled, Here is the social link https://www.quora.com/What-is-the-hostel-fees-and-mess-charge-in-VIT-approximately",
+            "rooms" : "Here is what I found. You can compare all the rooms in this link http://chennai.vit.ac.in/campus/hostelsfee",
+            "undergraduation" : "Which one do you like to know? Btech,BSC,BCOM,BCA? "+str(a),
+            "postgraduation" : "Which one do you like to know? Mtech,MSC,MCA,MBA? "+str(a),
+            "btech" : "Fees for B.Tech "+str(a),
+            "bcom" : "Fees for B.Com  "+str(a),
+            "bca" : "Fees for BCAis "+str(a),
+            "bsc" : "Fees for BSC  "+str(a),
+            "mtech" : "Fees for M.Tech  "+str(a),
+            "mcom" : "Fees for M.Com  "+str(a),
+            "msc" : "Fees for MSC  "+str(a),
+            "mba" : "Fees for MBA  "+str(a),
+            "fallback":"Sorry, I didn't get you.",
+            'placement':"Here is an excellent discussion on social media for that. https://www.quora.com/How-is-the-placement-in-VIT-Vellore-What-is-the-average-package", 
+            "help" :"Welcome TO VIT bot. \n You can ask questions from following topics. \n Fees \n Placements \n Academics \n Exam \n Hostel \n "
         }
         self.possible_states={
-            "initial":("hostel","academics"),
-            "fees":("hostel","academics"),
-            "hostel":("mess","rooms"),
-            "academics":("undergraduation","postgraduation"),
-            "undergraduation":("btech","bsc","bcom","bca"),
-            "postgraduation" : ("mtech","mcom","mca","mba")                        
+            #"placement": ("initial","help","fees"),
+            "initial":("hostel","academics","help","fees","undergraduation","postgraduation"),
+            "fees":("hostel","academics","help"),
+            "rooms":("help"),
+            "hostel":("mess","rooms","help"),
+            "academics":("undergraduation","postgraduation","help"),
+            "undergraduation":("btech","bsc","bcom","bca","help"),
+            "postgraduation" : ("mtech","mcom","mca","mba","help"),
+            "help":("fees", "postgraduation","undergraduation","academics","hostel")                      
         }
-        
+        self.machine.add_transition(trigger='fallback', source='initial', dest='initial')
+        self.machine.add_transition(trigger='fallback', source='fees', dest='fees')
+        self.machine.add_transition(trigger='fallback', source='hostel', dest='hostel')
+        self.machine.add_transition(trigger='fallback', source='academics', dest='academics')
+        self.machine.add_transition(trigger='fallback', source='mess', dest='mess')
+        self.machine.add_transition(trigger='fallback', source='rooms', dest='rooms')
+        self.machine.add_transition(trigger='fallback', source='undergraduation', dest='undergraduation')
+        self.machine.add_transition(trigger='fallback', source='postgraduation', dest='postgraduation')
+        self.machine.add_transition(trigger='fallback', source='final', dest='final')
+
         self.machine.add_transition(trigger='hostel', source='fees', dest='hostel')
         self.machine.add_transition(trigger='academics', source='fees', dest='academics')
         self.machine.add_transition(trigger='mess', source='hostel', dest='final')
@@ -231,165 +226,24 @@ class fees_FSM(object):
         self.machine.add_transition(trigger='mcom', source='postgraduation', dest='final')
         self.machine.add_transition(trigger='mca', source='postgraduation', dest='final')
         self.machine.add_transition(trigger='mba', source='postgraduation', dest='final')
-        if self.state=='initial':
-            print("self."+var1.intent(self.message.lower()).lower()+"()")
-            eval("self."+var1.intent(self.message.lower()).lower()+"()")
+        if 'help' in self.message.lower().split():
             print(self.state)
-            postmsg(self.room,self.questions[self.state],key)  
+            postmsg(self.room,self.questions["help"],key)  
 
-        '''
-        while(self.state!='final'):
-            answer=raw_input(self.questions[str(self.state)])
-            states=self.possible_states[self.state]
-            for possible_answer in states:
-                if possible_answer in answer:
-                    eval("self."+possible_answer+"()")
-                    if(self.state == 'final'):
-                        print self.questions[str(answer)]
-                    break
-        '''
-'''
-        while(self.state!='final'):
-            postmsg(self.room,self.questions[self.state],key)
-            states=self.possible_states[self.state]
-            for possible_answer in states:
-                if possible_answer in answer:
-                    eval("self."+possible_answer+"()")
-                    if(self.state == 'final'):
-                        print self.questions[str(answer)]
-                        break
-
-    class fees_FSM(object):
-
-        states=['inital','hostel','academics','mess','rooms','undergraduation','postgraduation','final']
-        def __init__(self, name):
-            self.name = name
-            self.questions={
-                "initial":"Would You like to Know Academics Fees or Hostel?: ",
-                "hostel":"Would You like to Know Mess Fees or Rooms?: ",
-                "academics":"Which Degree would you like to purse? UnderGraduation or PostGraduation?: ",
-                "mess": "Fees for mess in VIT is #to be filled",
-                "rooms" : "Here is what I found. You can compare all the rooms in this link",
-                "undergraduation" : "Which one do you like to know? Btech,BSC,BCOM,BCA?",
-                "postgraduation" : "Which one do you like to know? Mtech,MSC,MCA,MBA?",
-                "btech" : "Fees for B.Tech is #TBF",
-                "bcom" : "Fees for B.Com is #TBF",
-                "bca" : "Fees for BCAis #TBF",
-                "bsc" : "Fees for BSC is #TBF",
-                "mtech" : "Fees for M.Tech is #TBF",
-                "mcom" : "Fees for M.Com is #TBF",
-                "msc" : "Fees for MSC is #TBF",
-                "mba" : "Fees for MBA is #TBF",
-
-            }
-            self.possible_states={
-                "initial":("hostel","academics"),
-                "hostel":("mess","rooms"),
-                "academics":("undergraduation","postgraduation"),
-                "undergraduation":("btech","bsc","bcom","bca"),
-                "postgraduation" : ("mtech","mcom","mca","mba")                        
-            }
-            self.machine = Machine(model=self, states=fees_FSM.states, initial='initial')
-            self.machine.add_transition(trigger='hostel', source='initial', dest='hostel')
-            self.machine.add_transition(trigger='academics', source='initial', dest='academics')
-            self.machine.add_transition(trigger='mess', source='hostel', dest='final')
-            self.machine.add_transition(trigger='rooms', source='hostel', dest='final')
-            self.machine.add_transition(trigger='undergraduation', source='academics', dest='undergraduation')
-            self.machine.add_transition(trigger='postgraduation', source='academics', dest='postgraduation')
-
-            self.machine.add_transition(trigger='btech', source='undergraduation', dest='final')
-            self.machine.add_transition(trigger='bsc', source='undergraduation', dest='final')
-            self.machine.add_transition(trigger='bcom', source='undergraduation', dest='final')
-            self.machine.add_transition(trigger='bca', source='undergraduation', dest='final')
-
-            self.machine.add_transition(trigger='mtech', source='postgraduation', dest='final')
-            self.machine.add_transition(trigger='mcom', source='postgraduation', dest='final')
-            self.machine.add_transition(trigger='mca', source='postgraduation', dest='final')
-            self.machine.add_transition(trigger='mba', source='postgraduation', dest='final')
-            self.dialouge()
-        def dialouge(self):
-            while(self.state!='final'):
-                answer=raw_input(self.questions[str(self.state)])
-                states=self.possible_states[self.state]
-                for possible_answer in states:
-                    if possible_answer in answer:
-                        eval("self."+possible_answer+"()")
-                        if(self.state == 'final'):
-                            print self.questions[str(answer)]
-                        break
-
-'''
-
-'''
-class bot():
-	def __init__(self,botname,key,url):
-		self.botname=botname
-		self.key=key
-		self.url=url
-		self.name={}
-		self.var1=apiextraction("","yes")		
-		print("Algorithm Trained.")
-		#self.var1.save()
-		self.ws = websocket.WebSocketApp(self.url,on_message = self.on_message,on_error = self.on_error,
-		on_close = self.on_close,on_open=self.on_open)
-		print("Bot Running")
-		self.ws.run_forever()
-
-	def decodemsg(self,msgid):
-		r=requests.session()
-		r.headers["Content-Type"]="application/json; charset=utf-8"
-		r.headers["Authorization"]="Bearer " + self.key
-		response=r.get("https://api.ciscospark.com/v1/messages/"+msgid)
-		response=response.json()
-		text=response["text"].encode('utf-8')		
-		sender=response["personId"].encode('utf-8')
-		roomid=response["roomId"].encode('utf-8')		
-		return [text,sender,roomid]	
-	def postmsg(self,room,text):
-		p = requests.session()
-		p.headers["Content-Type"]="application/json; charset=utf-8"
-		p.headers["Authorization"]="Bearer "+self.key
-		payload={"text":str(text),"markdown":str(text),"roomId":str(room)}
-		res=p.post("https://api.ciscospark.com/v1/messages/",json=payload)	
-	def on_error(self,ws,error):
-		print(error)
-	def on_close(self,ws):
-		print("Bot Stopped")		
-	def on_message(self,ws,message):
-		try:
-			data=json.loads(message)
-		except:
-			data=""
-			botname=""
-			sender=""
-		finally:
-			if data:
-				botname=data["name"]
-				sender=data["data"]["personEmail"]
-				msgid=data["data"]["id"]
-				decoded=self.decodemsg(msgid)
-
-				message=str(decoded[0])
-				sender=str(decoded[1])
-				room=str(decoded[2])
-				print("Message: "+message+ "\nSender:  " +sender+"\nRoom:   " +room)
-				#self.postmsg(decoded[2],self.var1.intent(decoded[0]))
-				if sender in self.name.keys:
-					pass
-				else:
-					self.name[sender]='start'
-					mach=FSM()
-	def on_open(self,ws):
-		def run(*args):
-			ws.send("subscribe:"+self.botname)
-			while(1):
-				time.sleep(30)
-				ws.send("")
-			ws.close()
-			print("thread terminating...")
-		thread.start_new_thread(run, ())
-
-'''
+        if self.state=='initial':
+            self.state=var1.intent(self.message.lower()).lower()
+            if self.state=="fallback":
+                self.state='initial'
+            print(self.state)
+            if self.state== "placement":
+                self.state='initial'
+                postmsg(self.room,self.questions["placement"],key)
+                self.st+=1
+            if self.state=="initial" and st!=1:
+                postmsg(self.room,self.questions[self.state+"1"],key)
+            else:
+                postmsg(self.room,self.questions[self.state],key)
+                self.st+=1
 
 
 if __name__ == "__main__":
